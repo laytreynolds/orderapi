1ccb09aed3d59c7a9426999722409df2e4f3640a
Layton Reynolds <laytreynolds@hotmail.com>
2024-09-11 17:24:45 +0100

    uuid Package

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module orderapi
 
 go 1.20
+
+require github.com/lithammer/shortuuid v3.0.0+incompatible
+
+require github.com/google/uuid v1.6.0 // indirect
--- handler/update.go
+++ handler/update.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"log"
+	"net/http"
+)
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received %s request to update", r.Method)
+
+	// Check Method
+	if r.Method != http.MethodPut {
+
+	}
+}
