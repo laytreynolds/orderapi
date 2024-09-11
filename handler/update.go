95d728f6e8473b3ff7e277bbf96d94c40d90f3a2
Layton Reynolds <laytreynolds@hotmail.com>
2024-09-11 17:25:41 +0100

    Start update function

--- handler/update.go
+++ handler/update.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"log"
 	"net/http"
+	"orderapi/helpers"
 )
 
 func Update(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +11,21 @@ func Update(w http.ResponseWriter, r *http.Request) {
 
 	// Check Method
 	if r.Method != http.MethodPut {
-
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		log.Printf("HTTP Method %s Not Allowed", r.Method)
+		return
 	}
+
+	// Extract the query parameter "customer"
+	queryValues := r.URL.Query()
+	customer := queryValues.Get("customer")
+
+	// Search the orders array for orders matching the customer name
+	matchingOrders := []helpers.Order{}
+	for _, order := range helpers.Orders {
+		if order.Customer == customer {
+
+		}
+	}
+
 }
