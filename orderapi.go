ecd1294f7f4fd581dd45abb7ba1fa49ebab89ffb
Layton Reynolds <laytreynolds@hotmail.com>
2024-09-11 14:21:09 +0100

    Main Functions

--- orderapi.go
+++ orderapi.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"orderapi/handler"
+)
+
+var port = ":8080"
+
+func main() {
+
+	// Create an order
+	http.HandleFunc("/create", handler.Create)
+
+	// Get Order
+	http.HandleFunc("/get", handler.Get)
+
+	log.Printf("Listening on %s...", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
