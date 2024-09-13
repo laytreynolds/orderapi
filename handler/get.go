a123f7fa169c37be05103f49cf5b8870236321cd
Layton Reynolds <laytreynolds@hotmail.com>
2024-09-13 09:51:57 +0100

    Get All Orders

--- handler/get.go
+++ handler/get.go
@@ -56,3 +56,38 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	orderJSON, _ := json.MarshalIndent(matchingOrders, "", "    ")
 	log.Printf("Found Orders for %s:\n%s\n", customer, string(orderJSON))
 }
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	// Log the incoming request
+	log.Println("Received a request to retrieve all orders")
+
+	// Check Method
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		log.Printf("HTTP Method %s Not Allowed", r.Method)
+		return
+	}
+
+	// Set headers
+	w.Header().Set("Content-Type", "application/json")
+
+	// Check if orders is empty
+	if len(helpers.Orders) == 0 {
+		http.Error(w, "No orders found", http.StatusNotFound)
+		log.Printf("No orders found")
+		return
+	}
+
+	// Parse orders
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(helpers.Orders); err != nil {
+		log.Println("Failed to encode response:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	count := len(helpers.Orders)
+
+	orderJSON, _ := json.MarshalIndent(helpers.Orders, "", "    ")
+	log.Printf("Found %d Orders:\n%s\n", count, string(orderJSON))
+}
--- orderapi.go
+++ orderapi.go
@@ -16,6 +16,7 @@ func main() {
 	http.HandleFunc("/get", handler.Get)
 	http.HandleFunc("/update", handler.Update)
 	http.HandleFunc("/delete", handler.Delete)
+	http.HandleFunc("/getall", handler.GetAll)
 
 	log.Printf("Listening on %s...", port)
 	log.Fatal(http.ListenAndServe(port, nil))
