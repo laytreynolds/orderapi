b512405005255ab5d1b8221bdd140d281c81b654
Layton Reynolds <laytreynolds@hotmail.com>
2024-09-20 12:18:44 +0100

    tcp creation. Case switch for server types

---
+++ http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"orderapi/handler"
+)
+
+func RunHTTPServer() {
+	http.HandleFunc("/create", handler.Create)
+	http.HandleFunc("/get", handler.Get)
+	http.HandleFunc("/update", handler.Update)
+	http.HandleFunc("/delete", handler.Delete)
+	http.HandleFunc("/getall", handler.GetAll)
+
+	log.Printf("Listening on %s...", port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- orderapi.go
+++ orderapi.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"orderapi/handler"
+	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -11,14 +11,18 @@ var (
 )
 
 func main() {
+	// Define a command-line flag for the server type
+	t := flag.String("type", "", "Type of server to run (udp, tcp, http)")
+	flag.Parse()
 
-	http.HandleFunc("/create", handler.Create)
-	http.HandleFunc("/get", handler.Get)
-	http.HandleFunc("/update", handler.Update)
-	http.HandleFunc("/delete", handler.Delete)
-	http.HandleFunc("/getall", handler.GetAll)
-
-	log.Printf("Listening on %s...", port)
-
-	log.Fatal(http.ListenAndServe(port, nil))
+	// Check the server type and call the corresponding function
+	switch *t {
+	case "tcp":
+		runTCPServer()
+	case "http":
+		RunHTTPServer()
+	default:
+		fmt.Println("Please specify a valid server type: udp, tcp, or http")
+		os.Exit(1)
+	}
 }
